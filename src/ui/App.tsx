31437ffb9d02e72139f37f7c9c70a0b37e091005
Hanif <haniffadhlurrahman1999@gmail.com>
2025-12-24 04:58:44 +1100

    add electron and its builder

--- src/ui/App.tsx
+++ src/ui/App.tsx
@@ -1,6 +1,7 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
+import electronLogo from './assets/electron.svg'
 import './App.css'
 
 function App() {
@@ -9,6 +10,9 @@ function App() {
   return (
     <>
       <div>
+        <a href="https://electronjs.org" target="_blank">
+          <img src={electronLogo} className="logo" alt="Electron logo" />
+        </a>
         <a href="https://vite.dev" target="_blank">
           <img src={viteLogo} className="logo" alt="Vite logo" />
         </a>
@@ -16,7 +20,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Electron + Vite + React</h1>
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
